979fe13c27fe7a556f8e725a0be947d4134d328d
lazyYC <yuchilin0515@gmail.com>
2022-01-10 05:18:45 +0800

    code modularization

---
+++ classSeq2Seq.py
@@ -0,0 +1,64 @@
+class Seq2Seq(FairseqEncoderDecoderModel):
+    def __init__(self, args, encoder, decoder):
+        super().__init__(encoder, decoder)
+        self.args = args
+    
+    def forward(
+        self,
+        src_tokens,
+        src_lengths,
+        prev_output_tokens,
+        return_all_hiddens: bool = True,
+    ):
+        """
+        Run the forward pass for an encoder-decoder model.
+        """
+        encoder_out = self.encoder(
+            src_tokens, src_lengths=src_lengths, return_all_hiddens=return_all_hiddens
+        )
+        logits, extra = self.decoder(
+            prev_output_tokens,
+            encoder_out=encoder_out,
+            src_lengths=src_lengths,
+            return_all_hiddens=return_all_hiddens,
+        )
+        return logits, extra
+
+def build_model(args, task):
+    """ 按照參數設定建置模型 """
+    src_dict, tgt_dict = task.source_dictionary, task.target_dictionary
+
+    # 詞嵌入
+    encoder_embed_tokens = nn.Embedding(len(src_dict), args.encoder_embed_dim, src_dict.pad())
+    decoder_embed_tokens = nn.Embedding(len(tgt_dict), args.decoder_embed_dim, tgt_dict.pad())
+    
+    # 編碼器與解碼器
+    # TODO: 替換成 TransformerEncoder 和 TransformerDecoder
+    encoder = TransformerEncoder(args, src_dict, encoder_embed_tokens)
+    decoder = TransformerDecoder(args, tgt_dict, decoder_embed_tokens)
+    
+    # 序列到序列模型
+    model = Seq2Seq(args, encoder, decoder)
+    
+    def init_params(module):
+        from fairseq.modules import MultiheadAttention
+        if isinstance(module, nn.Linear):
+            module.weight.data.normal_(mean=0.0, std=0.02)
+            if module.bias is not None:
+                module.bias.data.zero_()
+        if isinstance(module, nn.Embedding):
+            module.weight.data.normal_(mean=0.0, std=0.02)
+            if module.padding_idx is not None:
+                module.weight.data[module.padding_idx].zero_()
+        if isinstance(module, MultiheadAttention):
+            module.q_proj.weight.data.normal_(mean=0.0, std=0.02)
+            module.k_proj.weight.data.normal_(mean=0.0, std=0.02)
+            module.v_proj.weight.data.normal_(mean=0.0, std=0.02)
+        if isinstance(module, nn.RNNBase):
+            for name, param in module.named_parameters():
+                if "weight" in name or "bias" in name:
+                    param.data.uniform_(-0.1, 0.1)
+            
+    # 初始化模型
+    model.apply(init_params)
+    return model
---
+++ cleanse.py
@@ -0,0 +1,64 @@
+def strQ2B(ustring):
+    """把字串全形轉半形"""
+    # 參考來源:https://ithelp.ithome.com.tw/articles/10233122
+    ss = []
+    for s in ustring:
+        rstring = ""
+        for uchar in s:
+            inside_code = ord(uchar)
+            if inside_code == 12288:  # 全形空格直接轉換
+                inside_code = 32
+            elif (inside_code >= 65281 and inside_code <= 65374):  # 全形字元（除空格）根據關係轉化
+                inside_code -= 65248
+            rstring += chr(inside_code)
+        ss.append(rstring)
+    return ''.join(ss)
+                
+def clean_s(s, lang):
+    if lang == 'en':
+        s = re.sub(r"\([^()]*\)", "", s) # remove ([text])
+        s = s.replace('-', '') # remove '-'
+        s = re.sub('([.,;!?()\"])', r' \1 ', s) # keep punctuation
+    elif lang == 'zh':
+        s = strQ2B(s) # Q2B
+        s = re.sub(r"\([^()]*\)", "", s) # remove ([text])
+        s = s.replace(' ', '')
+        s = s.replace('—', '')
+        s = s.replace('“', '"')
+        s = s.replace('”', '"')
+        s = s.replace('_', '')
+        s = re.sub('([。,;!?()\"~「」])', r' \1 ', s) # keep punctuation
+    s = ' '.join(s.strip().split())
+    return s
+
+def len_s(s, lang):
+    if lang == 'zh':
+        return len(s)
+    return len(s.split())
+
+def clean_corpus(prefix, l1, l2, ratio=9, max_len=1000, min_len=1):
+    if Path(f'{prefix}.clean.{l1}').exists() and Path(f'{prefix}.clean.{l2}').exists():
+        print(f'{prefix}.clean.{l1} & {l2} exists. skipping clean.')
+        return
+    with open(f'{prefix}.{l1}', 'r') as l1_in_f:
+        with open(f'{prefix}.{l2}', 'r') as l2_in_f:
+            with open(f'{prefix}.clean.{l1}', 'w') as l1_out_f:
+                with open(f'{prefix}.clean.{l2}', 'w') as l2_out_f:
+                    for s1 in l1_in_f:
+                        s1 = s1.strip()
+                        s2 = l2_in_f.readline().strip()
+                        s1 = clean_s(s1, l1)
+                        s2 = clean_s(s2, l2)
+                        s1_len = len_s(s1, l1)
+                        s2_len = len_s(s2, l2)
+                        if min_len > 0: # remove short sentence
+                            if s1_len < min_len or s2_len < min_len:
+                                continue
+                        if max_len > 0: # remove long sentence
+                            if s1_len > max_len or s2_len > max_len:
+                                continue
+                        if ratio > 0: # remove by ratio of length
+                            if s1_len/s2_len > ratio or s2_len/s1_len > ratio:
+                                continue
+                        print(s1, file=l1_out_f)
+                        print(s2, file=l2_out_f)
